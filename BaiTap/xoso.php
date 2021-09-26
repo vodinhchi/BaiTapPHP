01eea3ed4a94b5debf68227d1696f7ee51e6a0d7
vodinhchi <chi.vd.60cntt@ntu.edu.vn>
2021-09-26 21:36:45 +0700

    xoso.php

--- BaiTap/xoso.php
+++ BaiTap/xoso.php
@@ -1,41 +1,53 @@
+<style type="text/css">
+	th, td{
+    	border:1px solid gray;
+    	height: 40;
+	}
+	table{
+	    border-collapse:collapse;
+	    text-align: center;
+	    font-size: 20;
+	    font-family: Arial;
+	}
+</style>
+
 <h1 style="text-align: center">XỔ SỐ KHÁNH HÒA</h1>
-<table border="1px">
+<table>
 <tr>
-	<td>Giải 8</td>
-	<td style="color: red; font-size: 24; font-weight: bold;"><?php 
+	<td style="width:150px">Giải 8</td>
+	<td style="color: red; font-size: 24; font-weight: bold; width:150vw"><center><?php 
 		$X = random_int(0,99);
 		if(strlen($X)==1)	echo "0".$X;
 		else echo $X;
-	 ?></td>
+	 ?></center></td>
 </tr>
-<tr>
+<tr bgcolor="#DCDCDC">
 	<td>Giải 7</td>
 	<td><?php 
 		$X = random_int(0,999);
-		if(strlen($X)==1)	echo "00".$X;
+		if(strlen($X)==1)		echo "00".$X;
 		else if(strlen($X)==2)	echo "0".$X;
 		else echo $X;
 	 ?></td>
 </tr>
 <tr>
 	<td>Giải 6</td>
-	<?php 
+	<td><?php 
 	for ($i=0; $i < 3 ; $i++) { 
-		echo "<td>";
-			$X = random_int(0,9999);
-			if(strlen($X)==1)	echo "000".$X;
-			else if(strlen($X)==2)	echo "00".$X;
-			else if(strlen($X)==3)	echo "0".$X;
-			else echo $X;
-		echo "</td>";
+		$X = random_int(0,9999);
+		if(strlen($X)==1)		echo "&emsp;"."000".$X;
+		else if(strlen($X)==2)	echo "&emsp; "."00".$X;
+		else if(strlen($X)==3)	echo "&emsp; "."0".$X;
+		else echo "&emsp;".$X;
 	}
-	 ?>
+	 ?></td>
+	
 </tr>
-<tr>
+<tr bgcolor="#DCDCDC">
 	<td>Giải 5</td>
 	<td><?php 
 		$X = random_int(0,9999);
-		if(strlen($X)==1)	echo "000".$X;
+		if(strlen($X)==1)		echo "000".$X;
 		else if(strlen($X)==2)	echo "00".$X;
 		else if(strlen($X)==3)	echo "0".$X;
 		else echo $X;
@@ -43,50 +55,46 @@
 </tr>
 <tr>
 	<td>Giải 4</td>
-	<?php 
+	<td><?php 
 	for ($i=0; $i < 7 ; $i++) { 
-		echo "<td>";
-			$X = random_int(0,99999);
-			if(strlen($X)==1)	echo "0000".$X;
-			else if(strlen($X)==2)	echo "000".$X;
-			else if(strlen($X)==3)	echo "00".$X;
-			else if(strlen($X)==4)	echo "0".$X;
-			else echo $X;
-		echo "</td>";
+		$X = random_int(0,99999);
+		if(strlen($X)==1)		echo "&emsp;"."0000".$X;
+		else if(strlen($X)==2)	echo "&emsp;"."000".$X;
+		else if(strlen($X)==3)	echo "&emsp;"."00".$X;
+		else if(strlen($X)==4)	echo "&emsp;"."0".$X;
+		else echo "&emsp;".$X;
 	}
-	 ?>
+	 ?></td>
 </tr>
-<tr>
+<tr bgcolor="#DCDCDC">
 	<td>Giải 3</td>
-	<?php 
+	<td><?php 
 	for ($i=0; $i < 2 ; $i++) { 
-		echo "<td>";
-			$X = random_int(0,99999);
-			if(strlen($X)==1)	echo "0000".$X;
-			else if(strlen($X)==2)	echo "000".$X;
-			else if(strlen($X)==3)	echo "00".$X;
-			else if(strlen($X)==4)	echo "0".$X;
-			else echo $X;
-		echo "</td>";
+		$X = random_int(0,99999);
+		if(strlen($X)==1)		echo "&emsp;"."0000".$X;
+		else if(strlen($X)==2)	echo "&emsp;"."000".$X;
+		else if(strlen($X)==3)	echo "&emsp;"."00".$X;
+		else if(strlen($X)==4)	echo "&emsp;"."0".$X;
+		else echo "&emsp;".$X;
 	}
-	 ?>
+	 ?></td>
 </tr>
 <tr>
 	<td>Giải 2</td>
 	<td><?php 
 		$X = random_int(0,99999);
-		if(strlen($X)==1)	echo "0000".$X;
+		if(strlen($X)==1)		echo "0000".$X;
 		else if(strlen($X)==2)	echo "000".$X;
 		else if(strlen($X)==3)	echo "00".$X;
 		else if(strlen($X)==4)	echo "0".$X;
 		else echo $X;
 	 ?></td>
 </tr>
-<tr>
+<tr bgcolor="#DCDCDC">
 	<td>Giải 1</td>
 	<td><?php 
 		$X = random_int(0,99999);
-		if(strlen($X)==1)	echo "0000".$X;
+		if(strlen($X)==1)		echo "0000".$X;
 		else if(strlen($X)==2)	echo "000".$X;
 		else if(strlen($X)==3)	echo "00".$X;
 		else if(strlen($X)==4)	echo "0".$X;
@@ -94,10 +102,10 @@
 	 ?></td>
 </tr>
 <tr>
-	<td>Giải ĐB</td>
+	<td>ĐB</td>
 	<td style="color: red; font-size: 24; font-weight: bold;"><?php 
 		$X = random_int(0,999999);
-		if(strlen($X)==1)	echo "00000".$X;
+		if(strlen($X)==1)		echo "00000".$X;
 		else if(strlen($X)==2)	echo "0000".$X;
 		else if(strlen($X)==3)	echo "000".$X;
 		else if(strlen($X)==4)	echo "00".$X;
